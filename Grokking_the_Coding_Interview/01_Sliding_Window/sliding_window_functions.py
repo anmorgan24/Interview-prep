d930031a770c8c50a4ad54b5f5619e83a5b3b1ec
Abigail Morgan <anmorgan24@gmail.com>
2022-07-17 21:03:57 -0400

    smallest subarray with a greater sum

--- Grokking_the_Coding_Interview/01_Sliding_Window/sliding_window_functions.py
+++ Grokking_the_Coding_Interview/01_Sliding_Window/sliding_window_functions.py
@@ -16,7 +16,9 @@ def brute_force_max_sub_array_of_size_k(k, arr):
 
 
 
-# SLIDING WINDOW
+# SLIDING WINDOWS
+
+
 # Time complexity: O(N)
 # Space complexity: O(1)
 
@@ -34,3 +36,27 @@ def max_sub_array_of_size_k(k, arr):
             window_start += 1 # slide the window ahead
 
     return max_sum
+
+
+
+# Time complexity: O(N + N) = O(N) asymptotically 
+# Space complexity: O(1) (constant)
+
+import math
+
+def smallest_subarray_sum(s, arr):
+
+    min_length = math.inf
+    window_sum = 0
+    window_start = 0
+    for window_end in range(0, len(arr)):
+        window_sum += arr[window_end] # add the next element
+        # shrink the window as small as possible until the 'window_sum' is smaller than 's'
+        while window_sum >= s:
+            min_length = min(min_length, window_end = window_start +1)
+            window_sum -= arr[window_start]
+            window_start += 1
+    if min_length == math.inf:
+        return 0
+    return min_length
+
