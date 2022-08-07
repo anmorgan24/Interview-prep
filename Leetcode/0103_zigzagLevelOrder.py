1194ff26395416c8e01554053cedbd0a6c97b84e
Abigail Morgan <anmorgan24@gmail.com>
2022-08-07 16:16:26 -0400

    0103: zigZagLevelOrder.py

---
+++ Leetcode/0103_zigzagLevelOrder.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = []
+        if root is None:
+            return result
+        
+        queue = [(root)]
+        leftToRight = True
+        
+        while queue:
+            levelSize = len(queue)
+            currentLevel = []
+            for _ in range(levelSize):
+                currentNode = queue.pop(0)
+                
+                if leftToRight:
+                    currentLevel.append(currentNode.val)
+                else:
+                    currentLevel.insert(0, currentNode.val)
+                    
+                if currentNode.left:
+                    queue.append(currentNode.left)
+                if currentNode.right:
+                    queue.append(currentNode.right)
+                    
+            result.append(list(currentLevel))
+            
+            leftToRight = not leftToRight
+            
+            
+        return result
