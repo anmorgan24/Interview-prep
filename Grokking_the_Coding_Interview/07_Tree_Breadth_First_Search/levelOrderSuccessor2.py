17c4d7dee191f4f51eb513490417714a392f8aa3
Abigail Morgan <anmorgan24@gmail.com>
2022-08-25 20:39:40 -0400

    levelOrderSuccessor2.py

---
+++ Grokking_the_Coding_Interview/07_Tree_Breadth_First_Search/levelOrderSuccessor2.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+class TreeNode:
+    def __init(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+def find_successor(root, key):
+    if root is None:
+        return None
+
+    queue = []
+    queue.append(root)
+    while queue:
+        currentNode = queue.popleft()
+        #insert the children of the current node in the queue
+        if currentNode.left:
+            queue.append(currentNode.left)
+        if currentNode.right:
+            queue.append(currentNode.right)
+
+        # break if we have found the key
+        if currentNode.val == key:
+            break
+    
+    return queue[0] if queue else None
