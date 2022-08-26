5d6db5b90f11f405caac13dc7448f76498dec9b7
Abigail Morgan <anmorgan24@gmail.com>
2022-08-25 21:45:44 -0400

    find_permutation.py

---
+++ Grokking_the_Coding_Interview/01_Sliding_Window/find_permutation.py
@@ -0,0 +1,34 @@
+def find_permutation(str1, pattern):
+    window_start=0
+    matched=0
+    char_frequency={}
+    
+    for chr in pattern:
+        if chr not in char_frequency:
+            char_frequency[chr] = 0
+        char_frequency[chr] += 1
+
+    # 1/ Our goal is to matchall the characters from the 'char_frequency'
+    # with the current window
+    # 2/ Try to extend the range [window_start, window_end] as much as possible
+    for window_end in range(len(str1)):
+        right_char=str1[window_end]
+        if right_char in char_frequency:
+            
+            char_frequency[right_char] -= 1
+            if char_frequency[right_char] == 0:
+                matched +=1
+
+        if matched == len(char_frequency):
+            return True
+        
+        # shrink the window by one character 
+        if window_end >= len(pattern) -1:
+            left_char = str1[window_start]
+            window_start += 1
+            if left_char in char_frequency:
+                if char_frequency[left_char] == 0:
+                    matched -= 1
+                char_frequency[left_char] += 1
+        
+    return False
