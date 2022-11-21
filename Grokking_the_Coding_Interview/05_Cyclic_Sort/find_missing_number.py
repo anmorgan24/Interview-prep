13a77172b7b65f533bff3b7f88cb536dbb7266bf
Abigail Morgan <anmorgan24@gmail.com>
2022-11-20 19:50:44 -0500

    find_missing_number.py

---
+++ Grokking_the_Coding_Interview/05_Cyclic_Sort/find_missing_number.py
@@ -0,0 +1,12 @@
+def find_missing_number(nums):
+    i, n = 0, len(nums)
+    while i < n:
+        j = nums[i]
+        if nums[i] < n and nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]
+        else:
+            i += 1
+    for i in range(n):
+        if nums[i] != i:
+            return i
+    return n
