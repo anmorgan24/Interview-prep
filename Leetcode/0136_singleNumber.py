d5d28f76b4adbc320e454efd45eba09328620248
Abigail Morgan <anmorgan24@gmail.com>
2022-11-21 19:58:36 -0500

    0136: Find the single number

---
+++ Leetcode/0136_singleNumber.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 1:
+            return nums[0]
+        nums.sort()
+        for i in range(0, len(nums)-1, 2):
+            if nums[i] != nums[i+1]:
+                return nums[i]
+            elif nums[i + 2] == nums[-1]:
+                    return nums[-1]
