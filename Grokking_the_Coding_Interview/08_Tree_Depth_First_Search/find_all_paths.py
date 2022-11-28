d61771c283038efe04fc42667c9ec99f1c5e2e1c
Abigail Morgan <anmorgan24@gmail.com>
2022-11-27 21:57:25 -0500

    find_all_paths.py

---
+++ Grokking_the_Coding_Interview/08_Tree_Depth_First_Search/find_all_paths.py
@@ -0,0 +1,46 @@
+class TreeNode:
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+def find_paths(root):
+  allPaths = []
+  find_paths_recursive(root, [], allPaths)
+  return allPaths
+
+
+def find_paths_recursive(currentNode, currentPath, allPaths):
+  if currentNode is None:
+    return
+
+  # add the current node to the path
+  currentPath.append(currentNode.val)
+
+  # if the current node is a leaf and its value is equal to required_sum, save the current path
+  if currentNode.left is None and currentNode.right is None:
+    allPaths.append(list(currentPath))
+  else:
+    # traverse the left sub-tree
+    find_paths_recursive(currentNode.left, currentPath, allPaths)
+    # traverse the right sub-tree
+    find_paths_recursive(currentNode.right, currentPath, allPaths)
+
+  # remove the current node from the path to backtrack,
+  # we need to remove the current node while we are going up the recursive call stack.
+  del currentPath[-1]
+
+
+def main():
+
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(4)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  print(find_paths(root))
+
+
+main()
