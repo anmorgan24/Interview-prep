2e42076dba568d1247b66d0c585c4da9ddfd13b3
Abigail Morgan <anmorgan24@gmail.com>
2022-11-27 21:55:37 -0500

    0257: Binary Tree Paths

--- Leetcode/0257_binaryTreePaths.py
+++ Leetcode/0257_binaryTreePaths.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
 class Solution(object):
     def binaryTreePaths(self, root):
         """
@@ -31,3 +31,17 @@ class Solution(object):
             self.find_paths_recursive(currentNode.right, currentPath, allPaths)
             
         del currentPath[-1]
+
+def main():
+
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(4)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  required_sum = 23
+  print(str(binaryTreePaths(root)))
+
+
+main()
