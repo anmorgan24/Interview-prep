4c4d8162b0bab7c399f74de015c6eae5409101c5
Abigail Morgan <anmorgan24@gmail.com>
2022-12-05 19:50:31 -0500

    find_subsets_with_duplicates.py

--- Grokking_the_Coding_Interview/10_Subsets/find_subsets_with_duplicates.py
+++ Grokking_the_Coding_Interview/10_Subsets/find_subsets_with_duplicates.py
@@ -3,13 +3,15 @@ def find_subsets(nums):
   list.sort(nums)
   subsets = []
   subsets.append([])
-  startIndex, endIndex = 0, 0
+  #startIndex, endIndex = 0, 0
   for i in range(len(nums)):
-    startIndex = 0
+    #startIndex = 0
     # if current and the previous elements are same, create new subsets only from the subsets
     # added in the previous step
     if i > 0 and nums[i] == nums[i - 1]:
       startIndex = endIndex + 1
+    else:
+        startIndex = 0
     endIndex = len(subsets) - 1
     for j in range(startIndex, endIndex+1):
       # create a new subset from the existing subset and add the current element to it
